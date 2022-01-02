817316a3571e7310b4bbd9caadf7b93551211ff7
Lucas Feitosa Alves <lukasalves271@gmail.com>
2022-01-01 22:25:07 -0300

    feat:implement class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
+import { IsString, IsEmail, IsOptional } from "class-validator";
 import { User } from "../entities/user.entity";
 
 export class CreateUserDto extends User {
+  @IsString()
   name: string;
+
+  @IsEmail()
+  @IsOptional()
   email?: string;
 }
